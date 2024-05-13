eea570da2be2f632656f1f566ed31f553af3ea68
akada <akka_mrc_bm18@yahoo.co.jp>
2024-05-14 07:22:54 +0900

    harhat.configの編集

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,34 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.19",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: "0.8.17",
+        settings: {
+          viaIR: true,
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
+      },
+    ],
+  },
+  typechain: {
+    outDir: "frontend/types",
+    target: "ethers-v6",
+    alwaysGenerateOverloads: false,
+  },
 };
 
 export default config;
